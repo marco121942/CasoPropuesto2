2d83965d4ab6aa5e781f88354c3fb986a85d5524
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-08-19 05:02:53 -0500

    first commit

---
+++ Caso Propuesto2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Caso_Propuesto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DgClientes = new System.Windows.Forms.DataGridView();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.clientes = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REPORTE DE PEDIDOS POR CLIENTE";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // DgClientes
+            // 
+            this.DgClientes.AllowUserToAddRows = false;
+            this.DgClientes.AllowUserToDeleteRows = false;
+            this.DgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgClientes.Location = new System.Drawing.Point(78, 117);
+            this.DgClientes.Name = "DgClientes";
+            this.DgClientes.ReadOnly = true;
+            this.DgClientes.Size = new System.Drawing.Size(545, 238);
+            this.DgClientes.TabIndex = 1;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Location = new System.Drawing.Point(571, 388);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(35, 13);
+            this.txtTotal.TabIndex = 2;
+            this.txtTotal.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(502, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TOTAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "BUSCAR POR NOMBRE";
+            // 
+            // clientes
+            // 
+            this.clientes.FormattingEnabled = true;
+            this.clientes.Location = new System.Drawing.Point(256, 82);
+            this.clientes.Name = "clientes";
+            this.clientes.Size = new System.Drawing.Size(240, 21);
+            this.clientes.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clientes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.DgClientes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DgClientes;
+        private System.Windows.Forms.Label txtTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox clientes;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Caso Propuesto2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Caso_Propuesto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["caso2"].ConnectionString);
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void ListaClientes()
+        {
+            using (SqlDataAdapter df = new SqlDataAdapter("Listado_Clientes", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    df.Fill(Da, "Clientes");
+                    DgClientes.DataSource = Da.Tables["Clientes"];
+                    txtTotal.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+        public void Listar()
+        {
+            using (SqlDataAdapter df = new SqlDataAdapter("ClientesSelect", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataTable Da = new DataTable())
+                {
+                    df.Fill(Da);
+
+
+                    // clientes.DataSource = Da.Tables[0];
+                    for (int i =0; i<Da.Rows.Count; i++) {
+                        clientes.Items.Add(Da.Rows[i]["NombreContacto"]);
+                    }
+                   
+                   // clientes.Text = Da.Tables["Clientes"];
+
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaClientes();
+            Listar();
+        }
+        public void BusquedaClientes()
+        {
+            using (SqlDataAdapter Df = new SqlDataAdapter("Buscar_Cliente", cn))
+            {
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.Value = clientes.Text;
+                sqlParameter.SqlDbType = SqlDbType.VarChar;
+                sqlParameter.Size = 100;
+                sqlParameter.ParameterName = "@NombreContacto";
+
+                Df.SelectCommand.Parameters.Add(sqlParameter);
+                Df.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Clientes");
+                    DgClientes.DataSource = Da.Tables["Clientes"];
+                    txtTotal.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            BusquedaClientes();
+
+        }
+    }
+}
